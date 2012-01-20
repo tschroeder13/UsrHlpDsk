4a5265c8e6df6af00df3bdbcd93c9cd257b655f6
tschroeder13 <tobias.schroeder@gmx.eu>
2012-01-20 13:00:27 +0100

    ReviewController nutzt jetzt auch sauber den CheckService

--- grails-app/controllers/de/fhdortmund/UsrHlpDsk/checkUser/ReviewController.groovy
+++ grails-app/controllers/de/fhdortmund/UsrHlpDsk/checkUser/ReviewController.groovy
@@ -18,20 +18,15 @@ class ReviewController {
 
 	def runAllChecks = {
 		def model = Review.get(1)
-
 		
 		def runResult = []
 
 		def String searchValue = params.searchFor
 		def String searchAttribute = params.searchIn
 
-//		def sessionVar = DirectoryController.SESSION_VARIABLE_NAME
-//		def Map<String, InitialDirContext> contexts = session.getAttribute(sessionVar)
-//		def checks = Check.findAll(sort: 'weight', order: 'desc')
-
 		def checks = model.checks
 		checks.each(){ Check check ->
-			def tmp = CheckService.runCheck(searchAttribute, searchValue, check)
+			def tmp = CheckService.getInstance().runCheck(searchAttribute, searchValue, check)
 			
 			runResult.add(tmp)
 		}//each check
